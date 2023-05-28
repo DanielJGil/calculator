ccc59ecf1153214acd818bb9e7762bdf351b1679
Daniel <dannyjaygil@gmail.com>
2023-05-28 16:40:05 +0100

    Create functions to operate on values inputed

---
+++ script.js
@@ -0,0 +1,33 @@
+let num1;
+let num2;
+let oper;
+
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+const operate = (a, b, op) => {
+  if (op === "+") {
+    return add(a, b);
+  } else if (op === "-") {
+    return subtract(a, b);
+  } else if (op === "*") {
+    return multiply(a, b);
+  } else if (op === "/") {
+    return divide(a, b);
+  } else {
+    return "Invalid Input";
+  }
+};
