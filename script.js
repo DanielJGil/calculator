33c32c6c14b64a778c87ee2df6e863b622619b75
Daniel <dannyjaygil@gmail.com>
2023-06-08 09:43:08 +0100

    Add keyboard functionality

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 // Fix overflow of numbers, set limit
 
-///////////////////////
-
-// Add a “backspace” button, so the user can undo if they click the wrong number.
-
-// Add keyboard support!
-
 let num = "";
 let op = "";
 let sum = "";
@@ -60,6 +54,466 @@ const operate = (a, b, op) => {
     return "Invalid Input";
   }
 };
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "1") {
+    e.preventDefault();
+    num += "1";
+
+    display.textContent += 1;
+  } else if (e.key === "2") {
+    e.preventDefault();
+    num += "2";
+
+    display.textContent += 2;
+  } else if (e.key === "3") {
+    e.preventDefault();
+    num += "3";
+
+    display.textContent += 3;
+  } else if (e.key === "4") {
+    e.preventDefault();
+    num += "4";
+
+    display.textContent += 4;
+  } else if (e.key === "5") {
+    e.preventDefault();
+    num += "5";
+
+    display.textContent += 5;
+  } else if (e.key === "6") {
+    e.preventDefault();
+    num += "6";
+
+    display.textContent += 6;
+  } else if (e.key === "7") {
+    e.preventDefault();
+    num += "7";
+
+    display.textContent += 7;
+  } else if (e.key === "8") {
+    e.preventDefault();
+    num += "8";
+
+    display.textContent += 8;
+  } else if (e.key === "9") {
+    e.preventDefault();
+    num += "9";
+
+    display.textContent += 9;
+  } else if (e.key === "0") {
+    e.preventDefault();
+    num += "0";
+
+    display.textContent += 0;
+  } else if (e.key === "c" || e.key === "C") {
+    e.preventDefault();
+    num = "";
+    op = "";
+    sum = "";
+
+    display.textContent = "";
+  } else if (e.key === "b" || e.key === "B" || e.key === "Backspace") {
+    e.preventDefault();
+    num = num.substring(0, num.length - 1);
+
+    display.textContent = sum + op + num;
+  } else if (e.key === ".") {
+    e.preventDefault();
+    if (num.includes(".") === false) {
+      num += ".";
+      display.textContent += ".";
+    }
+  } else if (
+    e.key === "+" ||
+    e.key === "a" ||
+    e.key === "A" ||
+    e.key === "p" ||
+    e.key === "P"
+  ) {
+    e.preventDefault();
+    if (op === "-") {
+      sum -= Number(num);
+      sum = Number(sum);
+
+      op = "+";
+
+      if (sum !== Math.trunc(sum)) {
+        display.textContent = sum.toFixed(1);
+      } else {
+        display.textContent = sum;
+      }
+      display.textContent += "+";
+    } else if (op === "*") {
+      if (num === "") {
+        sum += Number(num);
+        sum = Number(sum);
+
+        op = "+";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+        } else {
+          display.textContent = sum;
+        }
+        display.textContent += "+";
+      } else {
+        sum *= Number(num);
+        sum = Number(sum);
+
+        op = "+";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+        } else {
+          display.textContent = sum;
+        }
+        display.textContent += "+";
+      }
+    } else if (op === "/") {
+      if (num === "") {
+        sum += Number(num);
+        sum = Number(sum);
+
+        op = "+";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+        } else {
+          display.textContent = sum;
+        }
+        display.textContent += "+";
+      } else {
+        sum /= Number(num);
+        sum = Number(sum);
+
+        op = "+";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+          display.textContent += "+";
+        } else if (op === "+" && num === 0) {
+          display.textContent = "BANK CARD DOWNLOADED";
+        } else {
+          display.textContent = sum;
+          display.textContent += "+";
+        }
+      }
+    } else {
+      sum += Number(num);
+      sum = Number(sum);
+
+      op = "+";
+
+      if (sum !== Math.trunc(sum)) {
+        display.textContent = sum.toFixed(1);
+      } else {
+        display.textContent = sum;
+      }
+      display.textContent += "+";
+    }
+
+    num = "";
+  } else if (
+    e.key === "-" ||
+    e.key === "s" ||
+    e.key === "S" ||
+    e.key === "m" ||
+    e.key === "M"
+  ) {
+    e.preventDefault();
+    if (op === "+") {
+      sum += Number(num);
+      sum = Number(sum);
+
+      op = "-";
+
+      if (sum !== Math.trunc(sum)) {
+        display.textContent = sum.toFixed(1);
+      } else {
+        display.textContent = sum;
+      }
+      display.textContent += "-";
+    } else if (op === "*") {
+      if (num === "") {
+        sum += Number(num);
+        sum = Number(sum);
+
+        op = "-";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+        } else {
+          display.textContent = sum;
+        }
+        display.textContent += "-";
+      } else {
+        sum *= Number(num);
+        sum = Number(sum);
+
+        op = "-";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+        } else {
+          display.textContent = sum;
+        }
+        display.textContent += "-";
+      }
+    } else if (op === "/") {
+      if (num === "") {
+        sum += Number(num);
+        sum = Number(sum);
+
+        op = "-";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+        } else {
+          display.textContent = sum;
+        }
+        display.textContent += "-";
+      } else {
+        sum /= Number(num);
+        sum = Number(sum);
+
+        op = "-";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+          display.textContent += "-";
+        } else if (op === "-" && num === 0) {
+          display.textContent = "BANK CARD DOWNLOADED";
+        } else {
+          display.textContent = sum;
+          display.textContent += "-";
+        }
+      }
+    } else {
+      if (sum === 0 || sum === "") {
+        sum += Number(num);
+        sum = Number(sum);
+
+        op = "-";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+        } else {
+          display.textContent = sum;
+        }
+        display.textContent += "-";
+      } else {
+        sum -= Number(num);
+        sum = Number(sum);
+
+        op = "-";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+        } else {
+          display.textContent = sum;
+        }
+        display.textContent += "-";
+      }
+    }
+    num = "";
+  } else if (
+    e.key === "*" ||
+    e.key === "m" ||
+    e.key === "M" ||
+    e.key === "t" ||
+    e.key === "T"
+  ) {
+    e.preventDefault();
+    if (op === "-") {
+      sum -= Number(num);
+      sum = Number(sum);
+
+      op = "*";
+
+      if (sum !== Math.trunc(sum)) {
+        display.textContent = sum.toFixed(1);
+      } else {
+        display.textContent = sum;
+      }
+      display.textContent += "*";
+    } else if (op === "+") {
+      sum += Number(num);
+      sum = Number(sum);
+
+      op = "*";
+
+      if (sum !== Math.trunc(sum)) {
+        display.textContent = sum.toFixed(1);
+      } else {
+        display.textContent = sum;
+      }
+      display.textContent += "*";
+    } else if (op === "/") {
+      if (num === "") {
+        sum += Number(num);
+        sum = Number(sum);
+
+        op = "*";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+        } else {
+          display.textContent = sum;
+        }
+        display.textContent += "*";
+      } else {
+        sum /= Number(num);
+        sum = Number(sum);
+
+        op = "*";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+          display.textContent += "*";
+        } else if (op === "*" && num === 0) {
+          display.textContent = "BANK CARD DOWNLOADED";
+        } else {
+          display.textContent = sum;
+          display.textContent += "*";
+        }
+      }
+    } else {
+      if (sum === 0 || sum === "" || num === "") {
+        sum += Number(num);
+        sum = Number(sum);
+
+        op = "*";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+        } else {
+          display.textContent = sum;
+        }
+        display.textContent += "*";
+      } else {
+        sum *= Number(num);
+        sum = Number(sum);
+
+        op = "*";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+        } else {
+          display.textContent = sum;
+        }
+        display.textContent += "*";
+      }
+    }
+
+    num = "";
+  } else if (e.key === "/" || e.key === "d" || e.key === "D") {
+    e.preventDefault();
+    if (op === "+") {
+      sum += Number(num);
+      sum = Number(sum);
+
+      op = "/";
+
+      if (sum !== Math.trunc(sum)) {
+        display.textContent = sum.toFixed(1);
+      } else {
+        display.textContent = sum;
+      }
+      display.textContent += "/";
+    } else if (op === "*") {
+      if (num === "") {
+        sum += Number(num);
+        sum = Number(sum);
+
+        op = "/";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+        } else {
+          display.textContent = sum;
+        }
+        display.textContent += "/";
+      } else {
+        sum *= Number(num);
+        sum = Number(sum);
+
+        op = "/";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+        } else {
+          display.textContent = sum;
+        }
+        display.textContent += "/";
+      }
+    } else if (op === "-") {
+      sum -= Number(num);
+      sum = Number(sum);
+
+      op = "/";
+
+      if (sum !== Math.trunc(sum)) {
+        display.textContent = sum.toFixed(1);
+      } else {
+        display.textContent = sum;
+      }
+      display.textContent += "/";
+    } else {
+      if (sum === 0 || sum === "" || num === "") {
+        sum += Number(num);
+        sum = Number(sum);
+
+        op = "/";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+        } else {
+          display.textContent = sum;
+        }
+        display.textContent += "/";
+      } else {
+        sum /= Number(num);
+        sum = Number(sum);
+
+        op = "/";
+
+        if (sum !== Math.trunc(sum)) {
+          display.textContent = sum.toFixed(1);
+          display.textContent += "/";
+        } else if (op === "/" && num === 0) {
+          display.textContent = "BANK CARD DOWNLOADED";
+        } else {
+          display.textContent = sum;
+          display.textContent += "/";
+        }
+      }
+    }
+
+    num = "";
+  } else if (
+    e.key === "Enter" ||
+    e.key === "=" ||
+    e.key === "e" ||
+    e.key === "E"
+  ) {
+    e.preventDefault();
+    num = Number(num);
+
+    sum = operate(sum, num, op);
+    if (sum !== Math.trunc(sum)) {
+      display.textContent = sum.toFixed(1);
+    } else if (op === "/" && num === 0) {
+      display.textContent = "BANK CARD DOWNLOADED";
+    } else {
+      display.textContent = sum;
+    }
+
+    op = "";
+    num = sum;
+    sum = "";
+  }
+});
 
 clear.addEventListener("click", () => {
   num = "";
@@ -326,7 +780,6 @@ subtractOp.addEventListener("click", () => {
 });
 
 multiplyOp.addEventListener("click", () => {
-  Math.trunc(sum);
   if (op === "-") {
     sum -= Number(num);
     sum = Number(sum);
